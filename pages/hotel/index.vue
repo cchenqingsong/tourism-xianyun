2f8f649b526dcf6adffe29f180d39f83904997e8
liyepai <liyejiano@qq.com>
2020-02-20 21:22:26 +0800

    '酒店首页静态结构'

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>酒店首页</div>
+  <div>
+    <el-row type="flex" class="herder">
+      <Search />
+      <Area />
+      <HotelSearch />
+      <Hotels/>
+    </el-row>
+  </div>
 </template>
 
 <script>
-export default {
+import Search from "../../components/hotel/search";
+import Area from "../../components/hotel/area";
+import HotelSearch from "../../components/hotel/hotelSearch";
+import Hotels from "../../components/hotel/hotels";
 
-}
+export default {
+  components: {
+    Search,
+    Area,
+    HotelSearch,
+    Hotels
+  }
+};
 </script>
 
-<style>
-
+<style scoped lang="less">
+.herder {
+  
+  width: 1000px;
+  margin: 0 auto;
+  flex-wrap: wrap;
+}
 </style>
